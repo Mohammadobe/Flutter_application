393dddc692e6f77441cd2ea7c7796f1f8742a455
Mohammadobe <154047952+Mohammadobe@users.noreply.github.com>
2024-02-04 14:08:39 +0300

    Scrollbar

---
+++ flutter_application_1/lib/Scrollbar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class firstStatelessWidget extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context ){
+
+    return MaterialApp(
+      home: Scaffold(
+
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Center(child: Icon(Icons.thumb_down)),
+        ),
+
+        body: Scrollbar(
+          thumbVisibility: true,
+          child: ListView(
+            children: [
+              Column(
+                children: [
+                  Text('BMW M5 Cs' , style: TextStyle(fontSize: 40)),
+                  Image.asset('assets/images/BMW.jpg' , height: 400,)
+                ],
+              ),
+            
+              Container(
+                child: Text('gggggggg'),
+              ),
+            
+              Container(
+                child: Text('Helloooo' , style: TextStyle(fontSize: 40)),
+              ),
+            
+              Container(
+                // Text('BMW M5 Cs' , style: TextStyle(fontSize: 40)),
+                child:Image.asset('assets/images/BMW.jpg' , height: 400,)
+              ),
+                
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+}
+
+void main(){
+  runApp(firstStatelessWidget());
+}
