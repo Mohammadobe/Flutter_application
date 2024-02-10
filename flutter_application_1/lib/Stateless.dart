736f2efd4f5dfc82589e8327e4a125aedbd6d1ce
Mohammadobe <154047952+Mohammadobe@users.noreply.github.com>
2024-02-10 16:34:52 +0300

    Stateless

--- flutter_application_1/lib/Stateless.dart
+++ flutter_application_1/lib/Stateless.dart
@@ -19,7 +19,22 @@ class firstStatelessWidget extends StatelessWidget{
           child: Column(
             children: [
               Text('BMW M5 Cs' , style: TextStyle(fontSize: 40)),
-              Image.asset('assets/Images/BMW.jpg' , height: 400,)
+              Image.asset('assets/Images/BMW.jpg' , fit: BoxFit.cover),
+
+              TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                  labelText: 'Search',
+                  hintText: 'Enter name car',
+                  icon: Icon(Icons.search)
+                ),
+              ),
+
+              Checkbox(
+                value: true,
+                onChanged: (x) => (),
+              ),
+         
             ],
           ),
         ),
