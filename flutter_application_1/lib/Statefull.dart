06178059f4f083c8ba639a7462557256a81b912f
Mohammadobe <154047952+Mohammadobe@users.noreply.github.com>
2024-02-10 15:31:08 +0300

    Statefull

--- flutter_application_1/lib/Statefull.dart
+++ flutter_application_1/lib/Statefull.dart
@@ -27,19 +27,29 @@ class ColorChange extends StatefulWidget{
 
 class _ColorChangeState  extends State<ColorChange>{
 
+  Color _color  = Colors.amber;
+  Color _newColor = Colors.red;
+  
+  String dropdownvalue = 'Red';
+  var items = [
+    'Red',
+    'Blue',
+    'Green',
+  ];
+
   @override
   Widget build(BuildContext context){
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red,
+        backgroundColor: _newColor,
         title: Center(
           child: Text("Color Changer")
         ),
       ),
 
       body: Container(
-        color: Colors.amber,
+        color: _color,
         child: Container(
           margin: EdgeInsets.only(top: 20),
           child: Center(
@@ -49,22 +59,85 @@ class _ColorChangeState  extends State<ColorChange>{
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    ElevatedButton(onPressed: () {},
-                    child: Text('Red')
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                      onPressed: () {
+                        setState(() {
+                          _color = Colors.red;
+                          _newColor = Colors.amber;
+                        });
+                      },
+                      child: Text('Red'),
                     ),
+
                     SizedBox(width: 20),
           
-                    ElevatedButton(onPressed: () {},
-                    child: Text('Blue')
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                      onPressed: () {
+                        setState(() {
+                          _color = Colors.blue;
+                          _newColor = Colors.red;
+                        });
+                      },
+                      child: Text('Blue'),
                     ),
+
                     SizedBox(width: 20),
           
-                    ElevatedButton(onPressed: () {},
-                    child: Text('Green')
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                      onPressed: () {
+                        setState(() {
+                          _color  = Colors.green;
+                          _newColor = Colors.blue; 
+                        });
+                      },
+                      child: Text('Green'),
                     ),
           
                   ],
-                )
+                ),
+
+                DropdownButton(
+                  focusColor: Colors.white,
+                  value: dropdownvalue,
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  // map => for loop for a list
+                  items: items.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Text(
+                        items,
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownvalue = newValue!;
+
+                      if (dropdownvalue == 'Red'){
+                        _color = Colors.red;
+                        _newColor = Colors.amber;
+                      }
+
+                      else if (dropdownvalue == 'Blue'){
+                        _color = Colors.blue;
+                        _newColor = Colors.red;
+                      }
+                      
+                      else if (dropdownvalue == 'Green'){
+                        _color = Colors.green;
+                        _newColor = Colors.blue;
+                      }
+                        
+                    });
+                  },
+                ),
+            
               ]  
             ),
           ),
